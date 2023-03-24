e57e2b3f9e00ef96d33e95d13d871738c77e8aa1
Arsenii Fomin <fomin.artmind@gmail.com>
2023-03-24 16:02:03 +0700

    simple Arsheim changes

--- EpicLoot/LegendarySystem/UniqueLegendaryHelper.cs
+++ EpicLoot/LegendarySystem/UniqueLegendaryHelper.cs
@@ -60,10 +60,12 @@ namespace EpicLoot.LegendarySystem
 
         public static IList<LegendaryInfo> GetAvailableLegendaries(ItemDrop.ItemData baseItem, MagicItem magicItem, bool rollSetItem)
         {
-            var availableLegendaries = LegendaryInfo.Values.Where(x => x.IsSetItem == rollSetItem && x.Requirements.CheckRequirements(baseItem, magicItem)).AddItem(GenericLegendaryInfo).ToList();
-            if (rollSetItem && availableLegendaries.Count > 1)
+            var availableLegendaries = LegendaryInfo.Values.Where(x => (!x.IsSetItem || rollSetItem) && x.Requirements.CheckRequirements(baseItem, magicItem)).ToList();
+                
+            if(availableLegendaries.Count == 0)
             {
-                availableLegendaries.Remove(UniqueLegendaryHelper.GenericLegendaryInfo);
+                availableLegendaries = new List<LegendaryInfo>();
+                availableLegendaries.AddItem(GenericLegendaryInfo);
             }
 
             return availableLegendaries;
