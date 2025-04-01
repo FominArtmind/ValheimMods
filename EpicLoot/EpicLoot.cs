15213e0aae4efcd0049fe26f59d0bf22306b69ae
Arsenii Fomin <fomin.artmind@gmail.com>
2025-04-01 17:01:35 +0700

    Updated to EpicLoot 0.10.7

--- EpicLoot/EpicLoot.cs
+++ EpicLoot/EpicLoot.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading;
 using AdventureBackpacks.API;
 using BepInEx;
 using BepInEx.Configuration;
